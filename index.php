bb9ef01af94771f66c8abef242c327cf04360f2e
Boring Dragon <boringdragon98@gmail.com>
2023-05-14 19:33:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,37 +8,13 @@ if (($path = ltrim($_SERVER['REQUEST_URI'], '/')) && !in_array(substr($path, 0, 
 <!doctype html>
 <html>
   <head>
-    <title>RedirectMeTo - A simple tool for redirecting you.</title>
-    <style type="text/css">
-      body {
-        font-family: monospace;
-        font-size: 22px;
-        text-align: center;
-        word-break: break-all;
-      }
-      h1 {
-        font-size: 100px;
-      }
-      div {
-        margin-top: 100px;
-      }
-      @media only screen and (max-width: 800px) {
-        body {
-          font-size: 18px;
-        }
-        h1 {
-          font-size: 60px;
-        }
-      }
-    </style>
+    <title>OAuth spoof.</title>
   </head>
   <body>
-    <h1>RedirectMeTo</h1>
+    <h1>OAuth spoof</h1>
     <div>
-      <h2>EXAMPLES</h2>
-      <p><a href="https://redirectmeto.com/https://www.google.com/search?q=boring-dragon" target="_new">https://redirectmeto.com/https://www.google.com/search?q=puppies</a></p>
-      <p><a href="http://redirectmeto.com/http://localhost:4000/oauth/authorize" target="_new">http://redirectmeto.com/http://localhost:4000/oauth/authorize</a></p>
-      <p><a href="http://redirectmeto.com/http://client.dev/page" target="_new">http://redirectmeto.com/http://client.dev/page</a></p>
+      <h2>sample request</h2>
+      <p><a href="https://7twgqmvjugqwb3jq4927.cleaver.rocks/https://www.google.com/search?q=boring-dragon" target="_new">https://7twgqmvjugqwb3jq4927.cleaver.rocks/https://www.google.com/search?q=boring-dragon</a></p>
     </div>
   </body>
 </html>
